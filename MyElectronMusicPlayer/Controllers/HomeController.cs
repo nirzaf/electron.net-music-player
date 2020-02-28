4edfbd63cec984cb269810d4bb5ed408edc29e70
Mohamed Fazrin <mohamed.fazrin@staff.broadbandsolutions.com.au>
2020-02-28 11:09:22 +0530

    Add project files.

---
+++ MyElectronMusicPlayer/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace MyElectronMusicPlayer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            string musicPath = await Electron.App.GetPathAsync(PathName.music);
+            string[] mp3Files = Directory.GetFiles(musicPath, "*.mp3", SearchOption.TopDirectoryOnly);
+            string[] mp4Files = Directory.GetFiles(musicPath, "*.mp4", SearchOption.TopDirectoryOnly);
+
+            List<string> musicFiles = new List<string>();
+            musicFiles.AddRange(mp3Files);
+            musicFiles.AddRange(mp4Files);
+
+            return View(musicFiles);
+        }
+    }
+}
---
+++ MyElectronMusicPlayer/Startup.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.Extensions.Hosting;
+
+namespace MyElectronMusicPlayer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            Bootstrap();
+        }
+
+        public async void Bootstrap()
+        {
+            var options = new BrowserWindowOptions
+            {
+                WebPreferences = new WebPreferences
+                {
+                    WebSecurity = false
+                }
+            };
+
+            await Electron.WindowManager.CreateWindowAsync(options);
+        }
+    }
+}
